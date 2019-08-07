2534917a813e5f57f7fcb4d7e5b7a75b4357b379
luizfper <luizfper@gmail.com>
2019-08-07 19:26:42 -0400

    Done.

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
   test 'gets created, assigns omniauth auth data to @auth' do
-    auth = {      
+    auth = {
       here: 'is',
       some: 'auth',
       data: '.',
